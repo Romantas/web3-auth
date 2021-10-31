6eecfc151253e57df3d44b9d154c5bce0dadb678
Romantas Trumpa <romantas.trumpa@kilo.health>
2021-10-31 23:14:21 +0200

    Finished auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,87 @@
-import "./App.css";
 import Web3 from "web3";
-import Web3Token from "web3-token";
-import axios from "axios";
+import "./App.css";
+
+let web3 = undefined; // Will hold the web3 instance
 
 function App() {
-  const connect = async () => {
-    // Connection to MetaMask wallet
-    const web3 = new Web3(Web3.givenProvider);
+  const handleAuthenticate = ({ publicAddress, signature }) =>
+    fetch(`http://localhost:8000/auth`, {
+      body: JSON.stringify({ publicAddress, signature }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    }).then((response) => response.json());
 
-    // getting address from which we will sign message
-    const address = (await web3.eth.getAccounts())[0];
+  const handleSignMessage = async ({ publicAddress, nonce }) => {
+    try {
+      const signature = await web3?.eth.personal.sign(
+        `I am signing my one-time nonce: ${nonce}`,
+        publicAddress,
+        "" // MetaMask will ignore the password argument here
+      );
 
-    // generating a token with 1 day of expiration time
-    const token = await Web3Token.sign(
-      (msg) => web3.eth.personal.sign(msg, address),
-      "1d"
-    );
+      return { publicAddress, signature };
+    } catch (err) {
+      throw new Error("You need to sign the message to be able to log in.");
+    }
+  };
 
-    axios.post(
-      "http://localhost:8000",
-      { name: "Adam" },
-      {
-        headers: {
-          Authorization: token,
-        },
+  const handleSignup = (publicAddress) =>
+    fetch(`http://localhost:8000/users`, {
+      body: JSON.stringify({ publicAddress }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+    }).then((response) => response.json());
+
+  const handleClick = async () => {
+    // Check if MetaMask is installed
+    if (!window.ethereum) {
+      window.alert("Please install MetaMask first.");
+      return;
+    }
+
+    if (!web3) {
+      try {
+        // Request account access if needed
+        await window.ethereum.enable();
+
+        // We don't know window.web3 version, so we use our own instance of Web3
+        // with the injected provider given by MetaMask
+        web3 = new Web3(window.ethereum);
+      } catch (error) {
+        window.alert("You need to allow MetaMask.");
+        return;
       }
-    );
+    }
+
+    const coinbase = await web3.eth.getCoinbase();
+    if (!coinbase) {
+      window.alert("Please activate MetaMask first.");
+      return;
+    }
+
+    const publicAddress = coinbase.toLowerCase();
+
+    // Look if user with current publicAddress is already present on backend
+    fetch(`http://localhost:8000/users?publicAddress=${publicAddress}`)
+      .then((response) => response.json())
+      // If yes, retrieve it. If no, create it.
+      .then((user) => (user?.nonce ? user : handleSignup(publicAddress)))
+      // Popup MetaMask confirmation modal to sign message
+      .then(handleSignMessage)
+      // Send signature to backend on the /auth route
+      .then(handleAuthenticate)
+      .catch((err) => {
+        window.alert(err);
+      });
   };
 
   return (
     <>
-      <button onClick={connect}>Connect</button>
+      <button onClick={handleClick}>Connect</button>
     </>
   );
 }
