d4db1d44885ea6ecac1bb1a5adfd0bfa481626cf
Romantas Trumpa <romantas.trumpa@kilo.health>
2021-10-31 23:13:55 +0200

    Finished auth

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,151 @@
 const express = require("express");
-const Web3Token = require("web3-token");
 const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const userModel = require("./models/User");
+const ethSig = require("eth-sig-util");
+const ethUtil = require("ethereumjs-util");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = 8000;
 
+app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", function (req, res) {
-  res.render("index", { title: "Hey", message: "Hello there!" });
+mongoose.connect("mongodb://localhost:27017/auth", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
 
-app.post("/", async function (req, res) {
-  const token = req.headers["authorization"];
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
 
-  const { address, body } = await Web3Token.verify(token);
+app.get("/users", function (req, res) {
+  userModel
+    .findOne({ where: { publicAddress: req.query.publicAddress } })
+    //--snip--
+    .then((user) => {
+      console.log(user);
+      if (!user) {
+        return res.status(404).send({ message: "User not found" });
+      }
+      return res.status(200).send(user);
+    });
+});
 
-  console.log(address, body);
+app.post("/users", async function (req, res) {
+  userModel
+    .create(req.body)
+    .then((user) => res.json(user))
+    .catch((e) => console.log(e));
+});
+
+app.post("/auth", async function (req, res, next) {
+  const { signature, publicAddress } = req.body;
+  if (!signature || !publicAddress)
+    return res
+      .status(400)
+      .send({ error: "Request should have signature and publicAddress" });
+
+  return (
+    userModel
+      .findOne({ where: { publicAddress } })
+      ////////////////////////////////////////////////////
+      // Step 1: Get the user with the given publicAddress
+      ////////////////////////////////////////////////////
+      .then((user) => {
+        if (!user) {
+          res.status(401).send({
+            error: `User with publicAddress ${publicAddress} is not found in database`,
+          });
+
+          return null;
+        }
+
+        return user;
+      })
+      ////////////////////////////////////////////////////
+      // Step 2: Verify digital signature
+      ////////////////////////////////////////////////////
+      .then((user) => {
+        if (!(user instanceof userModel)) {
+          // Should not happen, we should have already sent the response
+          throw new Error('User is not defined in "Verify digital signature".');
+        }
+
+        const msg = `I am signing my one-time nonce: ${user.nonce}`;
+
+        // We now are in possession of msg, publicAddress and signature. We
+        // will use a helper from eth-sig-util to extract the address from the signature
+        const msgBufferHex = ethUtil.bufferToHex(Buffer.from(msg, "utf8"));
+        const address = ethSig.recoverPersonalSignature({
+          data: msgBufferHex,
+          sig: signature,
+        });
+
+        // The signature verification is successful if the address found with
+        // sigUtil.recoverPersonalSignature matches the initial publicAddress
+        if (address.toLowerCase() === publicAddress.toLowerCase()) {
+          return user;
+        } else {
+          res.status(401).send({
+            error: "Signature verification failed",
+          });
+
+          return null;
+        }
+      })
+      ////////////////////////////////////////////////////
+      // Step 3: Generate a new nonce for the user
+      ////////////////////////////////////////////////////
+      .then((user) => {
+        if (!(user instanceof userModel)) {
+          // Should not happen, we should have already sent the response
+
+          throw new Error(
+            'User is not defined in "Generate a new nonce for the user".'
+          );
+        }
+
+        user.nonce = Math.floor(Math.random() * 10000);
+        return user.save();
+      })
+      ////////////////////////////////////////////////////
+      // Step 4: Create JWT
+      ////////////////////////////////////////////////////
+      .then((user) => {
+        return new Promise((resolve, reject) =>
+          // https://github.com/auth0/node-jsonwebtoken
+          jwt.sign(
+            {
+              payload: {
+                id: user.id,
+                publicAddress,
+              },
+            },
+            "secret",
+            {
+              algorithm: "HS256",
+            },
+            (err, token) => {
+              if (err) {
+                return reject(err);
+              }
+              if (!token) {
+                return new Error("Empty token");
+              }
+              return resolve(token);
+            }
+          )
+        );
+      })
+      .then((accessToken) => res.json({ accessToken }))
+      .catch(next)
+  );
 });
 
 app.listen(port, () => {
